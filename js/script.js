b4f6d7b617302949a7f22339a395191cdc1419e5
rolycore <rolycore@gmail.com>
2023-06-16 22:01:11 -0500

    ajuste en la barra de menu y otras funcionalidades

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+document.addEventListener('DOMContentLoaded',function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+});
 document.addEventListener('DOMContentLoaded', function() {
   // Función para resaltar el elemento de navegación activo
   const navLinks = document.querySelectorAll('nav a');
@@ -13,15 +21,15 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   // Función para mostrar/ocultar el menú de navegación en dispositivos móviles
-  const navToggle = document.querySelector('.nav-toggle');
-  const navMenu = document.querySelector('.nav-menu');
+  const navToggle = document.querySelector('#myTopnav');
+  const navMenu = document.querySelector('.menu');
 
   navToggle.addEventListener('click', function() {
     navMenu.classList.toggle('active');
   });
 
   // Función para cerrar el menú de navegación al hacer clic en un enlace en dispositivos móviles
-  const navMenuLinks = document.querySelectorAll('.nav-menu a');
+  const navMenuLinks = document.querySelectorAll('.menu a');
 
   navMenuLinks.forEach(function(link) {
     link.addEventListener('click', function() {
